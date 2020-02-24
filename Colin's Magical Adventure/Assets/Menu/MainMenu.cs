2526cb382b60fa306a1e98f3d6cdc1720948a468
TheMichel404 <61205514+TheMichel404@users.noreply.github.com>
2020-02-24 18:52:23 +0100

    Add main menu
    Add main menu
    - Load Scene
    - Graphics settings
    - Sound settings
    - Quit game

---
+++ Colin's Magical Adventure/Assets/Menu/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu: MonoBehaviour {
+
+    private void Start()
+    {
+        
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("VOUS AVEZ QUITTER LE JEU");
+    }
+}
